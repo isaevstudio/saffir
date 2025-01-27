c9d54e95d95f85ee9004a29fb2538da2d0308563
Parviz Isaev <parvizjan@inbox.ru>
2025-01-28 01:09:10 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ class Config(object):
     DEBUG=False
     TESTING = False
 
-    SECRET_KEY ='a9yf82b33072234619205345fcf10d49'
-    SMTP_PORT = 587
+    SECRET_KEY ='a9yf19205345fcf10d49'
+    SMTP_PORT = 586
     SMTP_SERVER = "smtp.gmail.com"
-    EMAIL_FROM = 'saffircutton@gmail.com'
-    # EMAIL_LIST = '992934446668@yandex.tj'
-    EMAIL_LIST = 'parvizjan@inbox.ru'
-    # PSWD = "saffircutton_$" # Original password
+    EMAIL_FROM = 'fircutt@gmail.com'
+    # EMAIL_LIST = '99293@yandex.tj'
+    EMAIL_LIST = 'pazj@mail.ru'
+    # PSWD = "saffir" # Original password
     PSWD = 'yhpg gkvc butv pgnq'
     
     SESSION_COOKIE_SECURE =True
@@ -16,8 +16,8 @@ class Config(object):
     UPLOAD_FOLDER = 'static/temp_save'
     REFRESH_DATA = 'data'
 
-    USR = 'adminSaffir'
-    PSW = 'Saffir2024_$'
+    USR = 'minfir'
+    PSW = 'fir2024_$'
 
     SESSION_PERMANENT = False
     SESSION_TYPE = "filesystem"
